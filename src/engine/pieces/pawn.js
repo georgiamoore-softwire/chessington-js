916f7da03e7659d1099447b3b26d27aca57d2ea1
georgiamoore-softwire <107865029+georgiamoore-softwire@users.noreply.github.com>
2022-06-27 10:11:27 +0100

    Added basic pawn movement

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from "../square";
+import Player from "../player";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,14 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = []
+        let location = board.findPiece(this)
+
+        if (this.player === Player.WHITE){
+            availableMoves.push(Square.at(location.row + 1,location.col));
+        } else if (this.player === Player.BLACK) {
+            availableMoves.push(Square.at(location.row - 1,location.col));
+        }
+        return availableMoves
     }
 }
