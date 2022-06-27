bc1ce51f65c12e908430ce7889bd34ea31bc4bed
georgiamoore-softwire <107865029+georgiamoore-softwire@users.noreply.github.com>
2022-06-27 10:37:17 +0100

    Added rook lateral movement rule

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +9,16 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = []
+        let location = board.findPiece(this)
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++){
+            if (i !== location.col) {
+                availableMoves.push(Square.at(location.row, i))
+            }
+            if (i !== location.row) {
+                availableMoves.push(Square.at(i, location.col))
+            }
+        }
+        return availableMoves
     }
 }
