006c5813abf451e597353ea2bbfd7f37a628e44c
georgiamoore-softwire <107865029+georgiamoore-softwire@users.noreply.github.com>
2022-06-27 11:14:15 +0100

    Added bishop movement rules

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,20 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = []
+        let location = board.findPiece(this)
+        let x = location.row
+        let y = location.col
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
+                if ((i !== x && j !== y)) { // check if this is the current location
+                    if (((i - x === j - y) || (i - x === -(j - y)))) { // check if new coordinate is on diagonal
+                        availableMoves.push(Square.at(i, j))
+                    }
+                }
+            }
+        }
+
+        return availableMoves
     }
 }
