ba2b5c994496a07fa947bddb74aebdd34fbe6bd2
georgiamoore-softwire <107865029+georgiamoore-softwire@users.noreply.github.com>
2022-06-27 10:19:49 +0100

    Added additional move rule for pawn

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -12,8 +12,14 @@ export default class Pawn extends Piece {
         let location = board.findPiece(this)
 
         if (this.player === Player.WHITE){
+            if (location.row === 1) { // first turn
+                availableMoves.push(Square.at(location.row + 2,location.col));
+            }
             availableMoves.push(Square.at(location.row + 1,location.col));
         } else if (this.player === Player.BLACK) {
+            if (location.row === 6) { // first turn
+                availableMoves.push(Square.at(location.row - 2,location.col));
+            }
             availableMoves.push(Square.at(location.row - 1,location.col));
         }
         return availableMoves
